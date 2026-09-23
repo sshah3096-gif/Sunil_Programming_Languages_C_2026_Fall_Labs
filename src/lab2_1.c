39288e63f8f7dfde7eb9c64033a3371f10b52a97
sshah3096-gif <sshah3096@gmail.com>
2026-09-23 08:36:13 +0000

    for loop but i need it to be done using while loop

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -12,16 +12,31 @@
 */
 
 int sum_to_n(int n) {
+    int sum=0;
+    for (int i = 1; i <=n; i++)
+    {
+      sum +=i;/* code */
+    }
     // TODO: implement sum with a for loop
-    return 0; // placeholder
+    return sum; // placeholder
 }
 
 int main(void) {
     int n;
+    int output;
 
     printf("Enter a positive integer n: ");
     scanf("%d", &n);
 
+    if (n<1)
+    {
+        printf("the integer must be greater than 0\n");/* code */
+    }
+    else{ 
+        printf("the sum of 1 to %d\n", n);
+        output = sum_to_n(n);
+        printf("the sum is %d\n",output);
+    }
     // TODO: validate input, call function, and print result
 
     return 0;
